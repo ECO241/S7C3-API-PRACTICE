5cfeca6b11df0dc932a11ca0a8ca23e64c47996f
josemoncada87 <josemoncada87@hotmail.com>
2024-03-15 14:44:02 -0500

    Update moduleNameMapper in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 // jest.config.js
 /** @type {import('jest').Config} */
+
 const config = {
   verbose: true,
   testEnvironment: "node",
   moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
-  moduleNameMapper: { "^@/(.*)$": "<rootDir>/src/$1" },
+  moduleNameMapper: { "^@/(.*)$": "<rootDir>/$1" },
 };
 
 module.exports = config;
